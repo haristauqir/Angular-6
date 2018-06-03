7a75a3593ddffcc02d8ea00565fd0cf6bef2f860
haristauqir <haristauqir@gmail.com>
2018-06-03 02:08:20 +0100

    adding models
    adding models

--- Recipe App - Angular /src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe App - Angular /src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+    new Recipe("A Test Recipe", "This is simply a test",
+     "https://realfood.tesco.com/media/images/Pomegranate-chicken-LGH-mini-a225fa35-1e18-4cb6-9fd0-b39911530e09-0-1400x919.jpg"),
+     new Recipe("A Test Recipe", "This is simply a test",
+     "https://realfood.tesco.com/media/images/Pomegranate-chicken-LGH-mini-a225fa35-1e18-4cb6-9fd0-b39911530e09-0-1400x919.jpg")
+    ];
 
   constructor() { }
 
